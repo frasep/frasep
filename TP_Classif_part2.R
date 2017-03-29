de49ef7a075716d536e8b2ab4c1d6a6a018a4866
CEPE-S1-14 <CEPE-S1-14@ensae.fr>
2017-03-29 10:57:46 +0200

    INit tp classif part 2

---
+++ TP_Classif_part2.R
@@ -0,0 +1,17 @@
+library(ClusterR)
+
+# CAH avec group kmeans et reaffectation individus
+setwd("C:/Users/Cepe-S1-14/REDCLA")
+donclassif2 <- read.csv("donclassif2.txt",sep=";")
+head(donclassif2)
+dim(donclassif2)
+plot(donclassif2)
+kmeansclust <- kmeans(donclassif2, 1000, iter.max=1)
+dim(kmeansclust$centers)
+
+distdon2 <- dist(kmeansclust$centers)
+myhclustdon2 <- hclust(distdon2, method = "single", members = NULL)
+plot(donclassif2,col=myhclustdon2$cluster+1)
+
+res <- cutree(myhclustdon2,7)
+plot(donclassif2,col=res)
